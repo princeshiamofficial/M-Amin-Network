ca53e73eb912a0196738ea6672b4c53c8874cf36
Prince Shiam <princeshiamofficial@users.noreply.github.com>
2026-07-03 09:39:03 +0600

    config: disable Next.js development static route indicator

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  devIndicators: {
+    appIsrStatus: false,
+  },
+} as any;
 
 export default nextConfig;
