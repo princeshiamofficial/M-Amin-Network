47f64a124069d1798d03cf3384fe3d619d5e6e0c
Prince Shiam <princeshiamofficial@users.noreply.github.com>
2026-07-12 02:57:59 +0600

    fix: change stroke-miterlimit to strokeMiterlimit to resolve React console warning

--- src/app/bill-payment/page.tsx
+++ src/app/bill-payment/page.tsx
@@ -247,7 +247,7 @@ export default function BillPayment() {
                       <>
                         {t("Pay Now", "পে নাও")}{" "}
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                          <path fill="none" strokeLinecap="square" stroke-miterlimit="10" strokeWidth="48" d="m268 112 144 144-144 144m124-144H100"></path>
+                          <path fill="none" strokeLinecap="square" strokeMiterlimit="10" strokeWidth="48" d="m268 112 144 144-144 144m124-144H100"></path>
                         </svg>
                       </>
                     )}
